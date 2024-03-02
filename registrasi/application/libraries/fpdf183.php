7803db14fb502ae2feec0bac20a16beb3303c3f6
DikaAntoniPutra <dikahanggara2@gmail.com>
2024-03-02 22:40:48 +0700

    dika update

--- registrasi/application/libraries/fpdf183.php
+++
@@ -1,8 +0,0 @@
-<?php
-class fpdf183 {
-
-    function __construct() {
-        include_once APPPATH . '/third_party/fpdf/fpdf.php';
-    }
-}
-?>
--- registrasi/application/views/layout/navigation.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    if ($this->session->userdata('auth')->id_role == '1' || $this->session->userdata('auth')->id_role == '2') {
-        $this->load->view('layout/navigation_admin') ;
-    } else if ($this->session->userdata('auth')->id_role == '4') {
-        $this->load->view('layout/navigation_common');
-    } else {
-        $this->load->view('layout/navigation_pengasuh');
-    }
-?>
